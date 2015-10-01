c17e14f95f967d03ba8a90a0454d51703ef27576
shurik111333 <chudov.aleksandr94@gmail.com>
2015-10-02 00:16:28 +0300

    calculate postfix

---
+++ 1semestr/3homework/3/3/stack.cpp
@@ -0,0 +1,52 @@
+struct StackNode
+{
+	double value;
+	StackNode * next;
+};
+
+struct Stack
+{
+	StackNode *top;
+};
+
+Stack *getNewStack()
+{
+	Stack *newStack = new Stack{ nullptr };
+	return newStack;
+}
+
+double getTop(Stack *stack)
+{
+	if (stack->top == nullptr)
+	{
+		return -1;
+	}
+	return stack->top->value;
+}
+
+double pop(Stack *stack)
+{
+	if (stack->top == nullptr)
+	{
+		return -1;
+	}
+	double topValue = stack->top->value;
+	StackNode *nextNode = stack->top->next;
+	delete stack->top;
+	stack->top = nextNode;
+	return topValue;
+}
+
+void push(Stack *stack, double newValue)
+{
+	StackNode *newElement = new StackNode{newValue, stack->top};
+	stack->top = newElement;
+}
+
+void clear(Stack *stack)
+{
+	while (stack->top != nullptr)
+	{
+		pop(stack);
+	}
+}

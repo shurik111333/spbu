ab4fc477edf01a0c42e7835b596e22042dac7893
shurik111333 <chudov.aleksandr94@gmail.com>
2015-12-07 13:31:22 +0300

    newOutput

--- 1semestr/10homework/1/tree.cpp
+++ 1semestr/10homework/1/tree.cpp
@@ -79,6 +79,16 @@ char **getCodes(Tree *tree)
     return codes;
 }
 
+void writeValue(char value, std::ostream &out)
+{
+    out << '\"';
+    if (value == '\n')
+        out << "\\n";
+    else
+        out << value;
+    out << "\" ";
+}
+
 void writeTree(TreeNode *node, std::ostream &out)
 {
     if (node == nullptr)
@@ -88,13 +98,13 @@ void writeTree(TreeNode *node, std::ostream &out)
     out << node->code << " ";
     if (node->isLetter)
     {
-        out << "\"" << node->value << "\" ";
+        writeValue(node->value, out);
         return;
     }
-    out << "(";
+    out << '(';
     writeTree(node->left, out);
     writeTree(node->right, out);
-    out << ")";
+    out << ')';
 
 }
 
